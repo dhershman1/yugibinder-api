44c89d87d076e517c4bf95dacebd2126697a18d7
dhershman1 <dustinh17@gmail.com>
2024-11-10 00:15:22 -0500

    Updated: Main index to run database connections

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 import express from 'express'
 import cors from 'cors'
 import session from 'express-session'
-import logger from 'pino-http'
+import pino from 'pino-http'
+import 'dotenv/config'
 
+// Routes
 import cardsRouter from './routes/cards.js'
+// Middleware
+import db from './middleware/db.js'
 
 const app = express()
 
 const PORT = process.env.PORT || 3001
 const SECRET = process.env.APP_SECRET || 'supercoolsecret'
 
+app.use(pino())
 app.use(cors())
 app.use(express.urlencoded())
 app.use(express.json())
-app.use(logger())
 app.use(
   session({
     secret: SECRET,
@@ -21,6 +25,30 @@ app.use(
     saveUninitialized: true
   })
 )
+
+if (process.env.DATABASE_URL || process.env.NODE_ENV === 'production') {
+  app.use(
+    db({
+      client: 'pg',
+      connection: process.env.DATABASE_URL,
+      searchPath: ['dusty_blog', 'public']
+    })
+  )
+} else {
+  app.use(
+    db({
+      client: 'pg',
+      searchPath: ['public'],
+      connection: {
+        port: process.env.DB_PORT,
+        host: process.env.DB_HOST,
+        user: process.env.DB_USER,
+        database: process.env.DB_NAME,
+        password: process.env.DB_PASS
+      }
+    })
+  )
+}
 
 app.use('/cards', cardsRouter)
 
