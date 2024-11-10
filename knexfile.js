294ec2f1dc94da9c0db5248a20354031c9838b78
dhershman1 <dustinh17@gmail.com>
2024-11-10 02:21:58 -0500

    Fixed: Knexfile password

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ export default {
   connection: {
     host: 'localhost',
     user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME
   },
   migrations: {
