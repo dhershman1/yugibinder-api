2f92106a757fa0b30664c3ace19754fac8586753
dhershman1 <dustinh17@gmail.com>
2024-11-14 11:48:35 -0500

    Added: port to knexfile for migrations

--- knexfile.js
+++ knexfile.js
@@ -4,6 +4,7 @@ export default {
   client: 'pg',
   connection: {
     host: 'localhost',
+    port: process.env.DB_PORT,
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     database: process.env.DB_NAME
